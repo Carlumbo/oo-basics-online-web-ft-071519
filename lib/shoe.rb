f1bfb52570681fc2163c91a5c8a925aaf0c8b4e2
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-22 21:48:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,11 +8,24 @@ class Shoe
   def condition
     @condition
   end 
+<<<<<<< HEAD
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
   def condition=(cobble)
     @condition = cobble 
+=======
+  def condition=(condition = "tattard ")
+    @condition = condition
+  end 
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+  def cobble=(@condition)
+    @condition = "new"
+    @cobble = @condition 
+    
+>>>>>>> 540ebd5c16529d4e54cfd424fbec904221ca2e16
   end 
 end 
